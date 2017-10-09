cbc0af75cb2bf54472a11dae283b10a87533efef
sumdy <395747812@qq.com>
2017-10-09 18:08:44 +0800

    app.py

---
+++ www/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'He Sheng'
+
+'''
+async web application.
+'''
+
+import logging;logging.basiConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+	return web.Response(boby=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+	app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+    
