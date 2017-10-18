98a130c7f42fb6b409bae2c1a30152d2894e71ed
sumdy <395747812@qq.com>
2017-10-18 17:13:43 +0800

    2222

--- www/orm.py
+++ www/orm.py
@@ -85,3 +85,179 @@ class Field(object):
 	def __str__(self):
 		return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
 		
+
+class StringField(Field):
+	"""docstring for StringField"""
+	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
+		
+
+class BooleanField(Field):
+	"""docstring for BooleanField"""
+	def __init__(self, name=None, default=False):
+		super().__init__(name, 'boolean',False, default)
+
+
+class IntegerField(Field):
+	"""docstring for IntegerField"""
+	def __init__(self, name=None, primary_key=False, default=0):
+		super().__init__(name, 'bigint',primary_key, default)
+
+
+class FloatField(Field):
+			"""docstring for FloatField"""
+	def __init__(self, name=None, primary_key=False, default=0.0):
+		super().__init__(name,'real', primary_key, default)					
+
+
+class TextField(Field):
+	"""docstring for FloatField"""
+	def __init__(self, name=None, default=None):
+		super().__init__(name, 'text', False, default)
+
+
+class ModelMetaclass(type):
+	"""docstring for ModelMetaclass"""
+	def __new__(cls, name, bases, attrs):
+		if name=='Model':
+			return type.__new__(cls,name,bases,attrs)
+		tableName = attrs.get('__table__',None) or name
+		logging.info('found model：%s (table：%s)' % (name, tableName))
+		
+		mappings = dict()
+		field = []
+		primaryKey = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info(' found mapping：%s ==> %s' % (k,v))
+				mappings[k] = v
+				if v.primary_key:
+					#找到主键
+					if primaryKey:
+						raise StandardError('Duplicate primary key for field：%s' % k)
+					primaryKey = k
+				else:
+					field.append(k)
+		if not primaryKey:
+			raise StandardError('Primary key not found.')
+		for k in mapping.keys():
+			attrs.pop(k)
+		ascaped_fields = list(map(lambda f:'`%s`' % f, fields))
+		attrs['__mappings__'] = mappings
+		attrs['__table__'] = tableName
+		attrs['__primary_key__'] = primaryKey
+		attrs['__fields__'] = fields
+		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey,', '.join(escaped_fields),tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values(%s)' % (tableName, ','.join(escaped_fields),primaryKey, create_args_string(len(escaped_fields) + 1))
+		attrs['__update__'] = 'update `%s` set where `%s`=?' % (tableName, ','.join(map(lambda f：'`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName,primaryKey)
+		return type.__new__(cls,name,bases,attrs)
+
+
+class MOdel(dict,metaclass=ModelMetaclass):
+	"""docstring for MOdel"""
+	def __init__(self, **kw):
+		super(MOdel, self).__init__(**kw)
+
+	
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	def getValue(self, key):
+		return getattr(self, key, None)
+
+	def getValueOrDefault(self, key):
+		value = getattr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s:%s' % (key, str(value)))
+				setatrr(self, key, value)
+		return value
+
+
+	@clasmethod
+	#查询多条记录
+	async def findAll(cls, where=None, args=None, **kw):
+		' find object by where clause.'
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+
+		orderBy = kw.get('orderBy',None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+
+		limit = kw.get('limit',None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit,int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple) ad len(limit) == 2:
+				sql.append('?,?')
+				sql.appen(limit)
+			else:
+				raise ValueError('Invalid limit value: %s', % str(limit))
+		rs = await select(' '.join(sql), args)
+		return [cls(**r) for r in rs]
+
+
+	@clasmethod
+	async def findNumber(cls, selecField, where=None, args=None):
+		' find number by select and where.'
+		sql = ['select %s _num_ from `%s`' % (selecField, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = await select(' '.join(sql), args,1)
+		if len(rs)==0:
+			return None
+			return rs[0]['_num_']
+
+
+	@clasmethod
+	#查询一条记录
+	async def find(cls, pk):
+		' find object by primary key. '
+		rs = await select('%s where `%s`=?' % (cls.__select__,cls.__primary_key__), [pk], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	#添加新记录
+	async def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = await execute(self.__insert__, args)
+		if rows !=1:
+			logging.warn('faield to insert record：affected row: %s' % rows)
+	#更新记录
+	async def update(self):
+		args = list(map(self.getValue, sel.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = await execute(self.__update__, args)
+		if rows !=1:
+			logging.warn('faield to update by primary key: affected rows: %s' % rows)
+
+
+	#删除记录
+	async def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = await execute(self.__delete__, args)
+		if rows !=1:
+			logging.warn('faield to remove by primary key: affected rows：%s' % rows)
+
+
